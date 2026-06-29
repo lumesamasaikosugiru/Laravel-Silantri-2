9f7b824572c89c6824e709e3efa64ec95e44961b
lumesamasaikosugiru <lumetrixsynf@gmail.com>
2026-06-29 13:52:41 +0700

    add: feature Wali Santri's Dashboard | fix: welcome page & admin view: Santri'Activities

--- app/Filament/Resources/SantriPermissions/SantriPermissionResource.php
+++ app/Filament/Resources/SantriPermissions/SantriPermissionResource.php
@@ -23,7 +23,7 @@ class SantriPermissionResource extends Resource
     protected static ?string $pluralModelLabel = 'Data Perizinan Santri';
     protected static string|UnitEnum|null $navigationGroup = "Santri's Activities";
 
-    protected static ?int $navigationSort = 3;
+    protected static ?int $navigationSort = 4;
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedClipboard;
     protected static ?string $recordTitleAttribute = 'santri_id';
